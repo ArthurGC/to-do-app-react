f103a9f7eee4fad49a20a8273eaca2a59ceaa2a0
arthurGC <aguzmanc@unsa.edu.pe>
2021-08-15 21:24:19 -0500

    Add refresh value state functionality

--- src/components/InputTodo.js
+++ src/components/InputTodo.js
@@ -1,10 +1,24 @@
 import React, { useState } from 'react';
 
 const InputTodo = () => {
-  const [state] = useState({ title: '' });
+  const [data, setData] = useState({ title: '' });
+
+  const onChange = (e) => {
+    setData({ [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
   return (
-    <form>
-      <input type="text" placeholder="Add Todo" value={state.title} />
+    <form onSubmit={(e) => handleSubmit(e)}>
+      <input
+        type="text"
+        placeholder="Add Todo"
+        value={data.title}
+        name="title"
+        onChange={(e) => onChange(e)}
+      />
       <button type="submit">Submit</button>
     </form>
   );
